2673c834e7e0fc3b87c32e990b28ff10682bac7d
enricokokot <51400198+enricokokot@users.noreply.github.com>
2021-12-19 10:03:25 +0100

    create and fail second test

--- arabicToRoman.test.js
+++ arabicToRoman.test.js
@@ -8,4 +8,8 @@ describe("Arabic to Roman", () => {
   it('Should return "I" when given 1', () => {
     expect(arabicToRoman(1)).toBe("I");
   });
+
+  it('Should return "II" when given 2', () => {
+    expect(arabicToRoman(2)).toBe("II");
+  });
 });
