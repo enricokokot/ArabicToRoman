b40c62513c4126fd60b18e4a60600c3d38f55b6c
enricokokot <51400198+enricokokot@users.noreply.github.com>
2021-12-19 10:12:31 +0100

    pass new tests with minimal change to code

--- arabicToRoman.js
+++ arabicToRoman.js
@@ -1,6 +1,9 @@
 function arabicToRoman(arabicNumber) {
   if (arabicNumber == 1) return "I";
-  else return "II";
+  if (arabicNumber == 2) return "II";
+  if (arabicNumber == 3) return "III";
+  if (arabicNumber == 4) return "IV";
+  if (arabicNumber == 5) return "V";
 }
 
 module.exports = { arabicToRoman };
