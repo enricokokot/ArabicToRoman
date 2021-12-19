bc689bb677d44561e1d5a67fed11e9a2be5f358a
enricokokot <51400198+enricokokot@users.noreply.github.com>
2021-12-19 09:59:08 +0100

    create and fail the first useful test

--- arabicToRoman.test.js
+++ arabicToRoman.test.js
@@ -4,4 +4,8 @@ describe("Arabic to Roman", () => {
   it("Initial test", () => {
     arabicToRoman();
   });
+
+  it('Should return "I" when given 1', () => {
+    expect(arabicToRoman(1)).toBe("I");
+  });
 });
