c02f3ae2aeb47c61de60d427ef853be1405e57da
enricokokot <51400198+enricokokot@users.noreply.github.com>
2021-12-19 14:06:37 +0100

    pass new tests by making function more versatile

--- arabicToRoman.js
+++ arabicToRoman.js
@@ -1,17 +1,32 @@
 function arabicToRoman(arabicNumber) {
   const romanNumerals = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
     10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+    0: "",
   };
-  return romanNumerals[arabicNumber];
+
+  answer = "";
+
+  while (arabicNumber > 0) {
+    goodNumber = Object.keys(romanNumerals)
+      .reverse()
+      .find((numero) => arabicNumber / numero >= 1);
+    arabicNumber -= goodNumber;
+    answer += romanNumerals[goodNumber];
+  }
+
+  return answer;
 }
 
 module.exports = { arabicToRoman };
