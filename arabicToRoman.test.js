94137efb04b79857afbdb25aa9f96a1ee8219091
enricokokot <51400198+enricokokot@users.noreply.github.com>
2021-12-19 10:09:56 +0100

    add and fail couple of new tests

--- arabicToRoman.test.js
+++ arabicToRoman.test.js
@@ -12,4 +12,16 @@ describe("Arabic to Roman", () => {
   it('Should return "II" when given 2', () => {
     expect(arabicToRoman(2)).toBe("II");
   });
+
+  it('Should return "III" when given 3', () => {
+    expect(arabicToRoman(3)).toBe("III");
+  });
+
+  it('Should return "IV" when given 4', () => {
+    expect(arabicToRoman(4)).toBe("IV");
+  });
+
+  it('Should return "V" when given 5', () => {
+    expect(arabicToRoman(5)).toBe("V");
+  });
 });
